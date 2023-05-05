ee108a658fb0f626a93e126d3347bf00317c4565
Yusra Habil <Bunksam177@gmail.com>
2023-05-05 12:17:48 +0800

    Create introduction to dart.dart

---
+++ introduction to dart.dart
@@ -0,0 +1,11 @@
+# Simple code to print by using data types. 
+# Simple code of string interpolation
+
+void main (){
+
+String name = "habil";
+int age = 17;
+double height = 16.0;
+
+print("hye my name is ${name} and i am ${age} years old");
+}
